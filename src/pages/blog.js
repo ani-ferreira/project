35d5a59fc2b087359e651055dc2b7d38c85baca6
anaferreira <estudioj.ferreira@gmail.com>
2021-08-10 22:33:19 -0300

    html

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,15 +3,13 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/Layout"
 
 const BlogPage = ({ data }) => {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { html } = markdownRemark
   return (
     <Layout pageTitle="My Blog Posts">
       {data.allMarkdownRemark.nodes.map(node => (
         <Link to={"/blog/"} key={node.id}>
           <h3>{node.frontmatter.title}</h3>
           <p>Posted: {node.frontmatter.date}</p>
-          <div dangerouslySetInnerHTML={{ __html: html }} />
+          <div dangerouslySetInnerHTML={{ __html: node.html }} />
         </Link>
       ))}
     </Layout>
