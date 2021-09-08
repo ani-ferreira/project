0591af804c2214cfe13cca3fc2842c30e5c6a10f
anaferreira <estudioj.ferreira@gmail.com>
2021-09-08 18:51:06 -0300

    update

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -9,11 +9,7 @@ const BlogIndex = ({ data, location }) => {
   if (posts.length === 0) {
     return (
       <Layout location={location} title={siteTitle}>
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
+        <p>No hay post para ver.</p>
       </Layout>
     )
   }
