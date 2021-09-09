7bf628ab1bd8ab809912f26fc76a2906b8ffdab4
anaferreira <estudioj.ferreira@gmail.com>
2021-09-08 21:11:20 -0300

    update

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
